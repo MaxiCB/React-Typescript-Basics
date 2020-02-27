96fb8e99aa9f9489509693cfca79b132a64017bf
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-27 00:08:59 -0600

    ADD Dependencies && ADD Mission Component
    - Added Axios && Ant Design
    - Implemented dynamic component for displaying mission data
    - Implemented Ant Design style
    - Implemented List component to handle all MissionCard Rendering

--- typescript-basics/src/App.tsx
+++ typescript-basics/src/App.tsx
@@ -1,36 +1,45 @@
 import * as React from 'react';
-import Button from './components/Button';
+// Axios
+import axios from 'axios'
+// Components
+import MissionCard from './components/MissionCard';
+// Ant Design
+import {List} from 'antd'
 
 interface Props {}
 
-interface State {
-  number: number;
+const App: React.FunctionComponent<Props> = (props) => {
+
+  const [apiData, setAPIData] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('https://api.spacexdata.com/v3/launches')
+      .then(res => setAPIData(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <div className="App">
+      <List
+      style={{margin: '20px'}}
+    grid={{
+      gutter: 16,
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 4,
+      xl: 4,
+      xxl: 6,
+    }}
+    dataSource={apiData}
+    renderItem={item => (
+      <List.Item >
+        <MissionCard launch={item}/>
+      </List.Item>
+    )}
+  />
+    </div>
+  );
 }
-export default class App extends React.Component<Props, State> {
 
-  state: State = {
-    number: 0
-  };
-
-  increment = () => {
-    this.setState({
-      number: (this.state.number + 1)
-    })
-  }
-
-  decrement = () => {
-    this.setState({
-      number: (this.state.number - 1)
-    })
-  }
-
-render() {
-    return (
-      <div className="App">
-        <p>{this.state.number}</p>
-        <Button title="Increment" function={this.increment}/>
-        <Button title="Decrement" function={this.decrement}/>
-      </div>
-    );
-  }
-}
+export default App
---
+++ typescript-basics/src/components/MissionCard.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+// Ant Design
+import { Card } from 'antd';
+
+interface Props {
+    launch: any
+}
+
+const { Meta } = Card;
+
+const MissionCard: React.FunctionComponent<Props> = (props) => {
+    console.log(props.launch)
+
+    const launchImage = props.launch.links.mission_patch_small
+    const missionName = props.launch.mission_name
+    const missionLink = props.launch.links.article_link
+
+    return (
+        <Card
+            style={{width: '240px'}}
+            cover={<img src={launchImage} alt={missionName}/>}
+            >
+            <Meta title={missionName} description={<a href={missionLink} rel="noopener noreferrer" target="_blank">Mission Link</a>}></Meta>
+        </Card>
+    )
+}
+
+export default MissionCard

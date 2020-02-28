7980329b06a4258fe88d6ef57a965a9462e87b9e
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-28 02:09:05 -0600

    ADD Layout && Page Header
    - Added Layout functionality
    - Added a header to the landing page
    - Need to move the list into its own component

--- typescript-basics/src/App.tsx
+++ typescript-basics/src/App.tsx
@@ -1,45 +1,54 @@
-import * as React from 'react';
+import * as React from "react";
 // Axios
-import axios from 'axios'
+import axios from "axios";
 // Components
-import MissionCard from './components/MissionCard';
+import MissionCard from "./components/MissionCard";
 // Ant Design
-import {List} from 'antd'
+import { Layout, List } from "antd";
+
+const { Header, Content } = Layout;
 
 interface Props {}
 
-const App: React.FunctionComponent<Props> = (props) => {
-
-  const [apiData, setAPIData] = React.useState([])
+const App: React.FunctionComponent<Props> = props => {
+  const [apiData, setAPIData] = React.useState([]);
 
   React.useEffect(() => {
-    axios.get('https://api.spacexdata.com/v3/launches')
+    axios
+      .get("https://api.spacexdata.com/v3/launches")
       .then(res => setAPIData(res.data))
-      .catch(err => console.log(err))
-  }, [])
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="App">
-      <List
-      style={{margin: '20px'}}
-    grid={{
-      gutter: 16,
-      xs: 1,
-      sm: 2,
-      md: 3,
-      lg: 4,
-      xl: 4,
-      xxl: 6,
-    }}
-    dataSource={apiData}
-    renderItem={item => (
-      <List.Item >
-        <MissionCard launch={item}/>
-      </List.Item>
-    )}
-  />
+      <Layout>
+        <Header>
+          <h2 style={{color: '#FFF'}}>SpaceX Explorer</h2>
+        </Header>
+        <Content style={{ padding: "0 50px", margin: '16px 0' }}>
+          <List
+            className="site-layout-content"
+            grid={{
+              gutter: 16,
+              xs: 1,
+              sm: 2,
+              md: 3,
+              lg: 4,
+              xl: 4,
+              xxl: 6
+            }}
+            dataSource={apiData}
+            renderItem={item => (
+              <List.Item>
+                <MissionCard launch={item} />
+              </List.Item>
+            )}
+          />
+        </Content>
+      </Layout>
     </div>
   );
-}
+};
 
-export default App
+export default App;
